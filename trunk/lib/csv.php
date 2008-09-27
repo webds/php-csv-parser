e1f636929cc693dd2cf9fb854864f9f468c96d01
kazu.dev <kazu.dev@0049f736-8c72-11dd-a205-0939b8ba2825>
2008-09-27 09:01:19 +0000

    import

---
+++ trunk/lib/csv.php
@@ -0,0 +1,369 @@
+<?php
+
+/**
+ * csv parser
+ *
+ * PHP version 5
+ *
+ * <code>
+ *
+ *   // usage sample
+ *   $csv = new csv;
+ *
+ *   // tell the object to parse a specific file
+ *   if ($csv->uses('my_file.csv')) {
+ *
+ *     // execute the following if given file is usable
+ *
+ *     // get the headers found in file
+ *     $array = $csv->headers();
+ *
+ *     // get a specific column from csv file
+ *     $csv->column($array[2]);
+ *
+ *     // get each record with its related header
+ *     // ONLY if all records length match the number
+ *     // of headers
+ *      if ($csv->symmetric()) {
+ *          $array = $csv->connect();
+ *      } else {
+ *          // fetch records that dont match headers length
+ *          $array = $csv->asymmetry();
+ *      }
+ *
+ *      // ignore everything and simply get the data as an array
+ *      $array = $csv->raw_array();
+ *   }
+ *
+ * <code>
+ *
+ * @version     $id$
+ * @copyright   1997-2005 The PHP Group
+ * @author      Kazuyoshi Tlacaelel <kazu.php@gmail.com>
+ * @license     PHP Version 3.0 {@link http://www.php.net/license/3_0.txt}
+ */
+class csv
+{
+    public
+
+    /**
+     * csv parsing default-settings
+     *
+     * @var     array
+     * @access  public
+     */
+    $settings = array(
+        'delimiter' => ',',
+        'eol' => ";",
+        'length' => 999999,
+        'escape' => '"'
+    );
+
+    private
+
+    /**
+     * imported data from csv
+     *
+     * @var     array
+     * @access  private
+     */
+    $_data = array(),
+
+    /**
+     * csv file to parse
+     *
+     * @var     string
+     * @access  private
+     */
+    $_filename = '',
+
+    /**
+     * csv headers to parse
+     *
+     * @var     array
+     * @access  private
+     */
+    $_headers = array();
+
+    /**
+     * csv file loader
+     *
+     * indicates the object which file is to be loaded
+     *
+     * @param   string  $filename
+     * @access  public
+     * @return  boolean true if file was loaded successfully
+     */
+    public function uses($filename)
+    {
+        $this->_filename = $filename;
+        return $this->_parse();
+    }
+
+    /**
+     * settings alterator
+     *
+     * lets you define different settings for scanning
+     *
+     * @param   mixed   $array
+     * @access  public
+     * @return  boolean true if changes where applyed successfully
+     */
+    public function settings($array)
+    {
+        $this->settings = array_merge($this->settings, $array);
+    }
+
+    /**
+     * header fetcher
+     *
+     * gets all headers into an array
+     *
+     * @access  public
+     * @return  array
+     */
+    public function headers()
+    {
+        return $this->_headers;
+    }
+
+    /**
+     * csv data extractor
+     *
+     * extracts csv data excluding the headers
+     *
+     * @access  public
+     * @return  void
+     */
+    public function data()
+    {
+        return $this->_data;
+    }
+
+    /**
+     * header and value connector
+     *
+     * Builds a connection for each record and its header
+     *
+     * Note: if the data is not symmetric an emty array will be
+     * returned instead.
+     *
+     * @access  public
+     * @return  array   fetches a collection of hashes like
+     * <code>
+     *   array (
+     *     array('header1' => 'value1', 'header2' => 'value2')
+     *   );
+     * </code>
+     */
+    public function connect()
+    {
+        if (!$this->symmetric()) return array();
+
+        $ret_arr = array();
+
+        foreach ($this->_data as $record) {
+            $item_array = array();
+            foreach ($record as $column => $value) {
+                $header = $this->_headers[$column];
+                $item_array[$header] = $value;
+            }
+            array_push($ret_arr, $item_array);
+        }
+
+        return $ret_arr;
+    }
+
+    /**
+     * data length/symmetry checker
+     *
+     * tells if the headers and all of the contents length match.
+     *
+     * @access  public
+     * @return  boolean
+     */
+    public function symmetric()
+    {
+        $hc = count($this->_headers);
+        foreach ($this->_data as $data) {
+            if (count($data) != $hc) return false;
+        }
+        return true;
+    }
+
+    /**
+     * asymmetric data fetcher
+     *
+     * gets the data that does not match the headers length
+     *
+     * @access  public
+     * @return  array
+     */
+    public function asymmetry()
+    {
+        $ret_arr = array();
+        $hc = count($this->_headers);
+        foreach ($this->_data as $data) {
+            if (count($data) != $hc) {
+                $ret_arr[] = $data;
+            }
+        }
+        return $ret_arr;
+    }
+
+    /**
+     * column fetcher
+     *
+     * gets all the data for a specific column identified by $name
+     *
+     * @param   mixed   string
+     * @access  public
+     * @return  array   like
+     * <code>
+     *   $array = $csv->column('header1');
+     *
+     *   // array
+     *   array(
+     *     'value1',
+     *     'value2',
+     *     'value3',
+     *   )
+     * </code>
+     */
+    public function column($name)
+    {
+        if (!in_array($name, $this->_headers)) return array();
+        $key = array_search($name, $this->_headers, true);
+        $ret_arr = array();
+        foreach ($this->_data as $data) {
+            $ret_arr[] = $data[$key];
+        }
+        return $ret_arr;
+    }
+
+    /**
+     * raw data as array
+     *
+     * @access  public
+     * @return  array
+     */
+    public function raw_array()
+    {
+        $ret_arr = array();
+        $ret_arr[] = $this->_headers;
+        $ret_arr[] = $this->_data;
+        return $ret_arr;
+    }
+
+    /**
+     * header creator
+     *
+     * uses prefix and creates a header for each column suffixed by a
+     * numeric value
+     *
+     * @param   string  $prefix
+     * @access  public
+     * @return  boolean fails if data is not symmetric
+     * @see     symmetric(), asymmetry()
+     */
+    public function create_headers($prefix)
+    {
+        if (!$this->symmetric()) return false;
+        $length = count($this->_headers) + 1;
+        $this->move_headers_to_data();
+
+        $ret_arr = array();
+        for ($i = 1; $i < $length; $i ++) {
+            $ret_arr[] = $prefix . "_$i";
+        }
+        $this->_headers = $ret_arr;
+        return $this->symmetric();
+    }
+
+    /**
+     * header injector
+     *
+     * uses a $list array filled with strings to fill headers to be
+     * used by this object.
+     *
+     * Note: that given $list must match the length of all rows and
+     * this must be symmetric.
+     *
+     * @param   array   $list
+     * @access  public
+     * @return  boolean fails if data is not symmetric
+     * @see     symmetric(), asymmetry()
+     */
+    public function inject_headers($list)
+    {
+        if (!$this->symmetric()) return false;
+        if (!is_array($list)) return false;
+        if (count($list) != count($this->_headers)) return false;
+        $this->_headers = $list;
+        return true;
+    }
+
+    /**
+     * csv parser
+     *
+     * reads csv data and transforms it into php-data
+     *
+     * @access  private
+     * @return  boolean
+     */
+    private function _parse()
+    {
+        if (! $this->_validates()) return false;
+
+        $a = array();
+        $c = 0;
+        $d = $this->settings['delimiter'];
+        $e = $this->settings['escape'];
+        $l = $this->settings['length'];
+
+        $res = fopen($this->_filename, 'r');
+        while ($keys = fgetcsv($res, $l, $d, $e)) {
+
+            if ($c == 0) $this->_headers = $keys;
+
+            array_push($a, $keys);
+            $c ++;
+        }
+        fclose($res);
+        unset($a[0]);
+        $this->_data = $a;
+        return true;
+    }
+
+    /**
+     * csv file validator
+     *
+     * checks wheather if the given csv file is valid or not
+     *
+     * @access  private
+     * @return  boolean
+     */
+    private function _validates()
+    {
+        # file existance
+        if (!file_exists($this->_filename)) return false;
+
+        # file extension
+        if (!preg_match('/\.csv$/i', $this->_filename)) return false;
+
+        return true;
+    }
+
+    private function move_headers_to_data()
+    {
+        $arr = array();
+        $arr[] = $this->_headers;
+        foreach ($this->_data as $row) {
+            $arr[] = $row;
+        }
+        $this->_data = $arr;
+        $this->_headers = array();
+    }
+}
+
+?>
---
+++ trunk/lib/csv_to_sql.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * csv to sql exporter
+ *
+ * Note: uses csv object
+ *
+ * @uses        csv
+ * @package
+ * @version     $Id:$
+ * @copyright   1997-2005 The PHP Group
+ * @author      Kazuyoshi Tlacaelel <kazu.php@gmail.com>
+ * @license     PHP Version 3.0 {@link http://www.php.net/license/3_0.txt}
+ */
+class csv_to_sql extends csv
+{
+
+    /**
+     * template for sql query.
+     *
+     * override this in a child class for multiple-database support
+     *
+     * @var     string
+     * @access  protected
+     */
+    protected $query_template = 'INSERT INTO %s (%s) VALUES (%s);';
+
+    /**
+     * sql dumper for symmetric data
+     *
+     * Exports csv data into sql. Only when the headers match all
+     * records length (symmetric)
+     *
+     * @param   strings $tablename the tablename to use for inserts
+     * @access  public
+     * @return  array
+     * @see     symmetric(), asymmetry()
+     */
+    public function create_queries($tablename)
+    {
+        $queries = array();
+        if (!$this->symmetric()) return $queries;
+        $headers = join(', ', $this->headers());
+
+        foreach ($this->data() as $record) {
+            $values = $this->join_record_values($record);
+            $queries[] = sprintf($this->query_template, $tablename, $headers,
+                $values);
+        }
+        return $queries;
+    }
+
+    public function join_record_values($record)
+    {
+        $values = array();
+        foreach ($record as $value) {
+            if ($this->convertable($value)) {
+                $value = mb_ereg_replace("/'/", '\'', $value);
+                $value = mb_convert_encoding($value, $this->from, $this->to);
+            } else {
+                $value = preg_replace("/'/", '\'', $value);
+            }
+            $values[] = "'" . $value . "'";
+        }
+        unset($record);
+        return join(', ', $values);
+    }
+
+    public function conversion($from, $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    public function convertable($string)
+    {
+        $encodings = mb_list_encodings();
+        if (!in_array($from, $encodings)) return false;
+        if (!in_array($to, $encodings)) return false;
+        if (!mb_check_encoding($string, $from)) return false;
+        return true;
+    }
+
+}
+
+?>
