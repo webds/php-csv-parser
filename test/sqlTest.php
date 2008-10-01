b329487c1ba49789155003a9f7cab147a56c64b8
kazu.dev <kazu.dev@0049f736-8c72-11dd-a205-0939b8ba2825>
2008-10-01 07:16:26 +0000

    added tests for sql compilation

--- test/sqlTest.php
+++ test/sqlTest.php
@@ -24,15 +24,33 @@ class sqlTest extends PHPUnit_Framework_TestCase
 
     public function testCreate_queries()
     {
+        $this->assertTrue($this->csv->uses('data/symmetric.csv'));
+        $expected = fix('symmetric_queries');
+        $this->assertEquals($expected, $this->csv->create_queries('test_table'));
     }
 
-    public function testConversion()
+    public function test_values_in_queries_must_not_have_trailing_spaces()
     {
+        $fname = 'data/symmetric_with_trailing_spaces.csv';
+        $this->assertTrue($this->csv->uses($fname));
+        $expected = fix('symmetric_queries');
+        $this->assertEquals($expected, $this->csv->create_queries('test_table'));
+    }
+
+    public function test_create_queries_with_alternate_columns()
+    {
+        $fname = 'data/symmetric.csv';
+        $this->assertTrue($this->csv->uses($fname));
+        $columns = array('header_a', 'header_c');
+        $res = $this->csv->create_queries('test_table', $columns);
+        $this->assertEquals(fix('alternated_header_queries'), $res);
     }
 
     public function testConvertable()
     {
     }
+
+
 }
 
 ?>
